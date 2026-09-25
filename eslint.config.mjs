9231eb645f7384c82c1fe8eec43e5486955d3558
yongt22 <yong.t@northeastern.edu>
2026-09-24 19:56:38 -0500

    Update ESLint to use class configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,58 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
 
-export default defineConfig([
+export default [
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2025,
+      },
+    },
+    plugins: {
+      prettier: prettier,
+    },
+
+    rules: {
+      // ESLint recommended rules
+      ...js.configs.recommended.rules,
+
+      indent: [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-console": 0,
+
+      // Prettier integration - this runs Prettier through ESLint
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "lf",
+          trailingComma: "es5",
+          singleQuote: false,
+        },
+      ],
+    },
   },
-]);
+  eslintConfigPrettier,
+];
